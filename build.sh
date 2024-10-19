69f268749c679268f0095e0216eebf6d83e5ac43
Fabio Nascimento <f43792@gmail.com>
2024-10-18 21:37:25 -0300

    First test @ server

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
+# docker build -t vectorlab/v3dviz:1.0 .
 docker build -t vectorlab/v3dviz:1.0 .
--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-docker run -p 3000:7100 -d v3dviz:1.0
+# docker run -p 3000:7100 -d vectorlab/v3dviz:1.0
+docker run -p 7100:7100 -d vectorlab/v3dviz:1.0
